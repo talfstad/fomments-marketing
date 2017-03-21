e15c02204291efc7c97a6af945d63eb9c196858e
Trevor Alfstad <trevor@buildcave.com>
2017-03-21 01:12:14 -0700

    reactive reducers separated from regular action based reducers

--- imports/reducers/index.js
+++ imports/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import sectionControls from './section-controls';
-import sections from './sections';
-import user from './user';
+import sectionControls from '/imports/reducers/section-controls';
+import sections from '/imports/reducers/reactive/sections';
+import user from '/imports/reducers/reactive/user';
 
 const rootReducer = combineReducers({
   sectionControls,
