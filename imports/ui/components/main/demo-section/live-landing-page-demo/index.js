615cc216a774acb3fb73fb37ec4f06ab1482127d
Trevor Alfstad <trevor@buildcave.com>
2017-10-02 14:02:00 -0700

    feat: landing page live demo. https://app.asana.com/0/0/440901255403938/f

--- imports/ui/components/main/demo-section/live-landing-page-demo/index.js
+++ imports/ui/components/main/demo-section/live-landing-page-demo/index.js
@@ -38,27 +38,20 @@ class LiveLandingPageDemo extends Component {
     return (
       <div
         ref={(c) => { this.el = c; }}
-        className="modal fade modal"
+        className="modal fade ohidden"
         tabIndex="-1"
       >
         <div className="tablet-container live-demo-modal" role="document">
           <div className="tablet-portrait">
             <div>
-              <video
-                autoPlay
-                loop
-                className="example-video"
-                height="540px"
-                poster="https://www.flowmail.com/images/index/poster.png"
-              >
-                <source src="/videos/fomments-demo-video.mov" type="video/mov;" />
-                <source src="/videos/fomments-demo-video.webm" type="video/webm; codecs=" />
-                  vp8, vorbis
-                <source src="/videos/fomments-demo-video.mp4" type="video/mp4; codecs=" />
-                  avc1.42E01E, mp4a.40.2
-              </video>
+              <iframe
+                src="/landing-page/index.html"
+              />
             </div>
           </div>
+          <button type="button" className="close-tablet-button close" data-dismiss="modal" aria-hidden="true">
+            <i className="fa fa-times fa-times-thin" />
+          </button>
         </div>
       </div>
     );
@@ -67,12 +60,10 @@ class LiveLandingPageDemo extends Component {
 
 LiveLandingPageDemo.propTypes = {
   history: PropTypes.shape({}),
-  modalTitle: PropTypes.string,
   modalRedirectRouteOnClose: PropTypes.string,
 };
 
 LiveLandingPageDemo.defaultProps = {
-  modalTitle: '',
   modalRedirectRouteOnClose: '/',
   history: {},
 };
